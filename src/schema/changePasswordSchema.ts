8a10c18d9e957e6fb301d33673e9228e0419ba87
Anar Safarov <anarsferov@gmail.com>
2023-09-25 12:49:16 +0400

    fix: password validation

--- src/schema/changePasswordSchema.ts
+++ src/schema/changePasswordSchema.ts
@@ -6,7 +6,7 @@ const changePasswordSchema = yup.object().shape({
     .required('Old password is required')
     .min(8, 'Password must be at least 8 characters long')
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]+$/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&()])[A-Za-z\d@$!%^*?&()]+$/,
       'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character'
     ),
   newPassword: yup
@@ -14,7 +14,7 @@ const changePasswordSchema = yup.object().shape({
     .required('New password is required')
     .min(8, 'Password must be at least 8 characters long')
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]+$/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&()])[A-Za-z\d@$!%^*?&()]+$/,
       'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character'
     )
     .notOneOf(
